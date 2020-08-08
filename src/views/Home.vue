dfb234fc1571279723eef956bc8873bca98a5f31
klubkovkv <kvklubkov@yandex.ru>
2020-08-08 17:43:45 +0500

    finish

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque excepturi, alias perspiciatis ex voluptas est totam a rerum ipsam atque.</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
